17e28fef7d9f0ed390014c2c7539a2475adb914d
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2021-04-27 17:57:40 +0200

    ResourceLogic stylecop finished

--- FFYLife/Logic/ResourceLogic.cs
+++ FFYLife/Logic/ResourceLogic.cs
@@ -1,26 +1,42 @@
-﻿using GameModel.Models;
-using StorageRepository;
-using System;
+﻿// <copyright file="ResourceLogic.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace Logic
 {
+    using GameModel.Models;
+    using StorageRepository;
+
+    /// <summary>
+    /// ResourceLogic class which implements the Save,Load methods.
+    /// </summary>
     public class ResourceLogic : ILogic
     {
         private IGameModel model;
         private StorageRepo repo;
 
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResourceLogic"/> class.
+        /// </summary>
+        /// <param name="model">The first parameter which is an IGameModel.</param>
+        /// <param name="repo">Second parameter which is a StorageRepo entity.</param>
         public ResourceLogic(IGameModel model, StorageRepo repo)
         {
             this.model = model;
             this.repo = repo;
         }
 
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResourceLogic"/> class.
+        /// </summary>
+        /// <param name="repo">parameter which is a StorageRepo entity.</param>
         public ResourceLogic(StorageRepo repo)
         {
             this.repo = repo;
         }
-        public GameModel.Models.GameModel LoadGame(string savefile)
+
+        public GameModel LoadGame(string savefile)
         {
             return repo.LoadGame(savefile);
         }
