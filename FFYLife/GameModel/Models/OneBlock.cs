f3fba8d826e1c75d25932e57aebba58938812020
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2021-04-28 12:31:03 +0200

    OneBlock stylecop done

--- FFYLife/GameModel/Models/OneBlock.cs
+++ FFYLife/GameModel/Models/OneBlock.cs
@@ -1,14 +1,19 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// <copyright file="OneBlock.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace GameModel.Models
 {
+    /// <summary>
+    /// OneBlock class.
+    /// </summary>
     public class OneBlock : GameItem
     {
-        
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OneBlock"/> class.
+        /// </summary>
+        /// <param name="cx">cx.</param>
+        /// <param name="cy">cy.</param>
         public OneBlock(double cx, double cy)
         {
             this.CX = cx;
