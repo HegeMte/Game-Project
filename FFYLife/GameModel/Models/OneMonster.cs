0d86a1f39f5e5005ecec08d749aba024f283a499
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2021-04-28 12:37:48 +0200

    OneMonster stylecop done

--- FFYLife/GameModel/Models/OneMonster.cs
+++ FFYLife/GameModel/Models/OneMonster.cs
@@ -1,31 +1,65 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// <copyright file="OneMonster.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace GameModel.Models
 {
+    /// <summary>
+    /// OneMonster class.
+    /// </summary>
     public class OneMonster : GameItem, IMonster
     {
-        public OneMonster(double cx, double cy , int monsterLvl)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OneMonster"/> class.
+        /// </summary>
+        /// <param name="cx">cx.</param>
+        /// <param name="cy">cy.</param>
+        /// <param name="monsterLvl">monsterlvl.</param>
+        public OneMonster(double cx, double cy, int monsterLvl)
         {
             this.CX = cx;
             this.CY = cy;
-            RewardCash = monsterLvl + 1;
-            Hp = monsterLvl * 2;
-            AttackDMG = monsterLvl;
-            AttackSpeed = 1; //todo
-            IsDead = false;
-            MonsterLVL = monsterLvl;
-
+            this.RewardCash = monsterLvl + 1;
+            this.Hp = monsterLvl * 2;
+            this.AttackDMG = monsterLvl;
+            this.AttackSpeed = 1;
+            this.IsDead = false;
+            this.MonsterLVL = monsterLvl;
         }
 
+        /// <summary>
+        /// Gets or sets the MonsterLVL.
+        /// </summary>
         public int MonsterLVL { get; set; }
+
+        /// <summary>
+        /// Gets or sets the RewardCash.
+        /// </summary>
         public int RewardCash { get; set; }
-        public int Hp { get ; set; }
-        public int AttackDMG { get ; set ; }
-        public int AttackSpeed { get ; set ; }
+
+        /// <summary>
+        /// Gets or sets the Hp.
+        /// </summary>
+        public int Hp { get; set; }
+
+        /// <summary>
+        /// Gets or sets the AttackDMG.
+        /// </summary>
+        public int AttackDMG { get; set; }
+
+        /// <summary>
+        /// Gets or sets the AttackSpeed.
+        /// </summary>
+        public int AttackSpeed { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether.
+        /// </summary>
         public bool IsDead { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether.
+        /// </summary>
         public bool IsStopped { get; set; }
     }
 }
