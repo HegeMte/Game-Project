755aad3cf49eb48f180d448a85b7a771591dca06
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2021-04-28 12:29:52 +0200

    IUnit interface stylecop done

--- FFYLife/GameModel/Models/IUnit.cs
+++ FFYLife/GameModel/Models/IUnit.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// <copyright file="IUnit.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace GameModel.Models
 {
+    /// <summary>
+    /// IUnit interface.
+    /// </summary>
     public interface IUnit
     {
-
+        /// <summary>
+        /// Gets or sets the Hp.
+        /// </summary>
         int Hp { get; set; }
+
+        /// <summary>
+        /// Gets or sets the AttackDMG.
+        /// </summary>
         int AttackDMG { get; set; }
 
+        /// <summary>
+        /// Gets or sets the AttackSpeed.
+        /// </summary>
         int AttackSpeed { get; set; }
     }
 }
