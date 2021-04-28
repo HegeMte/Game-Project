1a8bfe7f6ab3083bc7ebb29cce5493a6fa4bf6c5
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2021-04-28 11:43:55 +0200

    GameItem model stylecop finished

--- FFYLife/GameModel/GlobalSuppressions.cs
+++ FFYLife/GameModel/GlobalSuppressions.cs
@@ -7,3 +7,10 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<asdasdas", Scope = "member", Target = "~P:GameModel.Models.Chest.Answers")]
 [assembly: SuppressMessage("Design", "CA1002:Do not expose generic lists", Justification = "dasdas", Scope = "member", Target = "~P:GameModel.Models.Chest.Answers")]
+[assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:Fields should be private", Justification = "<Pending>", Scope = "member", Target = "~F:GameModel.Models.GameItem.area")]
+[assembly: SuppressMessage("Design", "CA1051:Do not declare visible instance fields", Justification = "<Pending>", Scope = "member", Target = "~F:GameModel.Models.GameItem.area")]
+[assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:Fields should be private", Justification = "<Pending>", Scope = "member", Target = "~F:GameModel.Models.GameItem.Rotdegree")]
+[assembly: SuppressMessage("Design", "CA1051:Do not declare visible instance fields", Justification = "<Pending>", Scope = "member", Target = "~F:GameModel.Models.GameItem.Rotdegree")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1623:Property summary documentation should match accessors", Justification = "<Pending>", Scope = "member", Target = "~P:GameModel.Models.GameItem.RealArea")]
+[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "<Pending>", Scope = "member", Target = "~M:GameModel.Models.GameItem.IsCollision(GameModel.Models.GameItem)~System.Boolean")]
+[assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1404:Code analysis suppression should have justification", Justification = "<Pending>")]
--- FFYLife/GameModel/Models/GameItem.cs
+++ FFYLife/GameModel/Models/GameItem.cs
@@ -1,40 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows.Media;
+﻿// <copyright file="GameItem.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace GameModel.Models
 {
+    using System.Windows.Media;
+
+    /// <summary>
+    /// GameItem abstract class.
+    /// </summary>
     public abstract class GameItem
     {
+        /// <summary>
+        /// Gets or sets  the Rotdegree.
+        /// </summary>
+        public double Rotdegree;
+
+        /// <summary>
+        /// Joins a first name and a last name together into a single string.
+        /// </summary>
         protected Geometry area;
 
-        public double CX { get; set; } 
+        /// <summary>
+        /// Gets or sets  the CX.
+        /// </summary>
+        public double CX { get; set; }
+
+        /// <summary>
+        /// Gets or sets  the CY.
+        /// </summary>
         public double CY { get; set; }
 
-
-        public double rotDegree;
-
-
+        /// <summary>
+        /// Gets or sets  the RealArea.
+        /// </summary>
         public Geometry RealArea
         {
-
             get
             {
                 TransformGroup tg = new TransformGroup();
-                tg.Children.Add(new TranslateTransform(CX, CY));
-                tg.Children.Add(new RotateTransform(rotDegree, CX, CY));
-                area.Transform = tg;
-                return area.GetFlattenedPathGeometry();
-
-
+                tg.Children.Add(new TranslateTransform(this.CX, this.CY));
+                tg.Children.Add(new RotateTransform(this.Rotdegree, this.CX, this.CY));
+                this.area.Transform = tg;
+                return this.area.GetFlattenedPathGeometry();
             }
         }
 
+        /// <summary>
+        /// IsCollision method.
+        /// </summary>
+        /// <param name="other">the parameter is a GameItem entity.</param>
+        /// <returns>a bool.</returns>
         public bool IsCollision(GameItem other)
         {
             return Geometry.Combine(this.RealArea, other.RealArea, GeometryCombineMode.Intersect, null).GetArea() > 0;
         }
-
     }
 }
